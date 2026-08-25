83495670e740644b894d3ae54cfade1823cbe302
Ivan Zhutyaev <192970727+IvanZhutyaev@users.noreply.github.com>
2026-08-25 17:14:50 +0300

    Setter Injection

--- spring-core/src/main/java/zhu/dev/Task.java
+++ spring-core/src/main/java/zhu/dev/Task.java
@@ -1,15 +1,20 @@
 package zhu.dev;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("main-task")
 public class Task {
+    private TaskManager taskManager;
     public Task() {
         this.name = "task";
         this.duration = 60L;
+        this.taskManager = taskManager;
     }
-
-
+    @Autowired
+    public void setTaskManager(TaskManager taskManager) {
+        this.taskManager = taskManager;
+    }
 
     public Long getDuration() {
         return duration;
