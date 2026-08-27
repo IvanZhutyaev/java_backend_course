12e2fa2a5c159e7a7605c0e134a8a358afba9beb
Ivan Zhutyaev <192970727+IvanZhutyaev@users.noreply.github.com>
2026-08-27 12:26:52 +0300

    Scope("prototype")

--- spring-core/src/main/java/zhu/dev/Main.java
+++ spring-core/src/main/java/zhu/dev/Main.java
@@ -5,18 +5,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     static void main() {
         AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(TaskConfiguration.class);
-//        Task task = context.getBean(Task.class);
-//        Task task2 = (Task)context.getBean("main-task");
-//
-//        System.out.println(task);
-//        System.out.println(task2);
-//        System.out.println(task==task2);
-
+                new AnnotationConfigApplicationContext("zhu.dev");
+        Task task1 = context.getBean(Task.class);
+        Task task2 = context.getBean(Task.class);
+        System.out.println("task1==task2: "+(task1==task2));
         TaskManager taskManager = context.getBean(TaskManager.class);
-        TaskManager taskManager2 = (TaskManager) context.getBean("taskManager");
-
         taskManager.printTask();
+
+       
 
 
     }
