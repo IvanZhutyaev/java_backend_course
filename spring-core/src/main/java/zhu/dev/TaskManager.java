ccd51f0c150507d37e6b2836cf42adbb92a34d8a
Ivan Zhutyaev <192970727+IvanZhutyaev@users.noreply.github.com>
2026-08-27 12:50:22 +0300

    hw of postconstruct and predestroy

--- spring-core/src/main/java/zhu/dev/TaskManager.java
+++ spring-core/src/main/java/zhu/dev/TaskManager.java
@@ -1,9 +1,13 @@
 package zhu.dev;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class TaskManager {
     private final Task task;
 
@@ -12,7 +16,14 @@ public class TaskManager {
         this.task = task;
     }
 
-
+    @PostConstruct
+    public void init(){
+        System.out.println("TaskManager init!!!");
+    }
+    @PreDestroy
+    public void destroy(){
+        System.out.println("TaskManager destroy!!!");
+    }
     public void printTask(){
         if(task==null){
             System.out.println("Not found task");
