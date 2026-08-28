60d53994ac7d879484ee3f35219ac4c95bf7c8c5
Ivan Zhutyaev <192970727+IvanZhutyaev@users.noreply.github.com>
2026-08-28 14:16:53 +0300

    add own advice

--- spring-core/src/main/java/zhu/dev/aop/LoggingAspect.java
+++ spring-core/src/main/java/zhu/dev/aop/LoggingAspect.java
@@ -1,6 +1,7 @@
 package zhu.dev.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
@@ -9,22 +10,33 @@ import java.lang.reflect.Executable;
 @Component
 @Aspect
 public class LoggingAspect {
-    @Before("execution(* zhu.dev.TaskManager.*(..))")
-    public void logBefore(JoinPoint joinPoint) {
-        System.out.println("Перед вызовом метода:"+joinPoint.getSignature().getName());
+//    @Before("execution(* zhu.dev.TaskManager.*(..))")
+//    public void logBefore(JoinPoint joinPoint) {
+//        System.out.println("Перед вызовом метода:"+joinPoint.getSignature().getName());
+//    }
+//
+//    @AfterReturning(value = "execution(* zhu.dev.TaskManager.*(..))", returning = "result")
+//    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+//        System.out.println("Псоле возвращение рещзультата:"+joinPoint.getSignature().getName()+" результат = "+result);
+//    }
+//    @AfterThrowing(value = "execution(* zhu.dev.TaskManager.*(..))", throwing = "exc")
+//    public void afterThrowing(JoinPoint joinPoint, Exception exc) {
+//        System.out.println("После исключения!@!!: "+ exc.getMessage());
+//    }
+//
+//    @After("execution(* zhu.dev.TaskManager.*(..))")
+//    public void after(JoinPoint joinPoint) {
+//        System.out.println("После выполнение метода: "+joinPoint.getSignature().getName());
+//    }
+
+    @Around("execution(* zhu.dev.TaskManager.*(..))")
+    public Object LogAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("Before method");
+        Object result = joinPoint.proceed();
+        System.out.println("After method");
+        return result;
     }
 
-    @AfterReturning(value = "execution(* zhu.dev.TaskManager.*(..))", returning = "result")
-    public void logAfterReturning(JoinPoint joinPoint, Object result) {
-        System.out.println("Псоле возвращение рещзультата:"+joinPoint.getSignature().getName()+" результат = "+result);
-    }
-    @AfterThrowing(value = "execution(* zhu.dev.TaskManager.*(..))", throwing = "exc")
-    public void afterThrowing(JoinPoint joinPoint, Exception exc) {
-        System.out.println("После исключения!@!!: "+ exc.getMessage());
-    }
 
-    @After("execution(* zhu.dev.TaskManager.*(..))")
-    public void after(JoinPoint joinPoint) {
-        System.out.println("После выполнение метода: "+joinPoint.getSignature().getName());
-    }
+
 }
